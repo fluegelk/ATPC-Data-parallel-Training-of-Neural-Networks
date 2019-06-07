cb2edd81f93b22ab810a0e3b830a17c07cfe7ee1
fluegelk <katharina.fluegel@student.kit.edu>
2019-06-07 13:32:28 +0200

    ADD: functions to load and store training checkpoints

--- implementation/training.py
+++ implementation/training.py
@@ -1,1 +1,19 @@
 # Train a given model on a given data set
+import torch
+
+
+def saveCheckpoint(path, net, optimizer, epoch):
+    torch.save({
+        'model_state_dict': net.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+        'epoch': epoch
+    }, path)
+
+
+def loadCheckpoint(path, net, optimizer):
+    checkpoint = torch.load(path)
+    net.load_state_dict(checkpoint['model_state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    epoch = checkpoint['epoch']
+
+    return epoch
