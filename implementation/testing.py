0ad776fa013d066d7994146bfe51ed7fff39b3c4
fluegelk <katharina.fluegel@student.kit.edu>
2019-06-07 13:31:32 +0200

    ADD: Computing accuracy on test set

--- implementation/testing.py
+++ implementation/testing.py
@@ -1,1 +1,39 @@
-# Test a given (trained) model on a given data set. Determine the total accuracy and the accuracy per class
+import torch
+
+
+def testAccuracy(net, testLoader, numClasses):
+    """
+    @brief      Test a given (trained) model on a set of test data. Determine total accuracy and accuracy per class.
+
+    @param      net         The trained model
+    @param      testLoader  The data loader for the test data
+    @param      numClasses  The number of different classes
+
+    @return     A tuple of the overall accuracy over all classes and a list of the accuracies per class
+    """
+    overall_correct = 0.
+    overall_total = 0.
+    class_correct = [0.] * numClasses
+    class_total = [0.] * numClasses
+    with torch.no_grad():
+        net.eval()
+        for data in testLoader:
+            images, labels = data
+            _, predicted = torch.max(net(images), 1)
+            correct = (predicted == labels).squeeze()
+            overall_total += len(labels)
+            overall_correct += correct.sum().item()
+            for i in range(len(labels)):
+                class_correct[labels[i]] += correct[i].item()
+                class_total[labels[i]] += 1
+
+    overall_accuracy = overall_correct / overall_total
+    accuracyByClass = [0.] * numClasses
+    for i in range(numClasses):
+        accuracyByClass[i] = class_correct[i] / class_total[i]
+    return (overall_accuracy, accuracyByClass)
+
+
+def printClassAccuracy(classNames, accuracyByClass):
+    for i in range(len(classNames)):
+        print('Accuracy of %5s : %2d %%' % (classNames[i], 100 * accuracyByClass[i]))
