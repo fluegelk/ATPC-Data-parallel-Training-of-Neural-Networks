04948ad14a88966f358eba94f3de6ad65e4d79eb
fluegelk <katharina.fluegel@student.kit.edu>
2019-06-07 13:49:49 +0200

    ADD: helper functions to convert labels to class names

--- implementation/visualisation.py
+++ implementation/visualisation.py
@@ -1,2 +1,19 @@
 # Display data sets as images with their correct and predicted labels
 # Plot learning rate and accuracies over time
+
+
+def labelsToClassName(labels, classes):
+    return list(map(lambda i: classes[i], labels))
+
+
+def actualVsPredictedClass(actualLabels, predictedLabels, classes):
+    """
+    Takes two lists of image labels (actual and predicted) and a list of class names.
+    Returns a list actual and predicted class names separated by a new line.
+    """
+    actualClasses = labelsToClassName(actualLabels, classes)
+    predictedClasses = labelsToClassName(predictedLabels, classes)
+
+    def formatString(actual, predicted):
+        return '{}\n{}'.format(actual, predicted)
+    return list(map(formatString, actualClasses, predictedClasses))
