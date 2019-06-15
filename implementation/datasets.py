87f6245c2637c3f64a07ad37002e0a60a1932627
fluegelk <katharina.fluegel@student.kit.edu>
2019-06-15 16:31:13 +0200

    EDIT: reorganize loading of datasets
    Move to datasets.py and split into loading the dataset and creating the dataloader

---
+++ implementation/datasets.py
@@ -0,0 +1,61 @@
+import torch
+import torchvision
+from enum import Enum
+
+
+class DataSet(Enum):
+    MNIST = 1
+    CIFAR10 = 2
+
+
+class PaddingWrapper(object):
+    """
+    Wrapper class for torchvision.transforms.functional.pad
+    Allows passing the pad function to torchvision.transforms.Compose
+    """
+
+    def __init__(self, padding, fill=0, padding_mode='constant'):
+        self.padding = padding
+        self.fill = fill
+        self.padding_mode = padding_mode
+
+    def __call__(self, img):
+        return torchvision.transforms.functional.pad(img, padding=self.padding, fill=self.fill,
+                                                     padding_mode=self.padding_mode)
+
+
+def loadTorchDatasetMNIST(root, download):
+    # pad MNIST 28x28 images with 2 px to reach same size as CIFAR (32x32)
+    transform = torchvision.transforms.Compose([
+        PaddingWrapper(padding=2, padding_mode='edge'),
+        torchvision.transforms.ToTensor()
+    ])
+    trainset = torchvision.datasets.MNIST(root=root, train=True, download=download, transform=transform)
+    testset = torchvision.datasets.MNIST(root=root, train=False, download=download, transform=transform)
+    classes = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+    return trainset, testset, classes
+
+
+def loadTorchMNIST(root, download, batch_size, num_workers):
+    trainset, testset, classes = loadTorchDatasetMNIST(root, download)
+    trainLoader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+    testLoader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+    return trainLoader, testLoader, classes
+
+
+def loadTorchDatasetCIFAR10(root, download):
+    transform = torchvision.transforms.Compose([
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+    trainset = torchvision.datasets.CIFAR10(root=root, train=True, download=download, transform=transform)
+    testset = torchvision.datasets.CIFAR10(root=root, train=False, download=download, transform=transform)
+    classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+    return trainset, testset, classes
+
+
+def loadTorchCIFAR10(root, download, batch_size, num_workers):
+    trainset, testset, classes = loadTorchDatasetCIFAR10(root, download)
+    trainLoader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+    testset = torchvision.datasets.CIFAR10(root=root, train=False, download=download, transform=transform)
+    return trainLoader, testLoader, classes
--- implementation/main.py
+++ implementation/main.py
@@ -9,68 +9,13 @@ import training
 import testing
 import models
 import visualisation as vis
+from datasets import *
 
 
 class Model(Enum):
     PyTorchTutorialNet = 1
     LeNet5 = 2
     LeNet5Updated = 3
-
-
-class DataSet(Enum):
-    MNIST = 1
-    CIFAR10 = 2
-
-
-class PaddingWrapper(object):
-    """
-    Wrapper class for torchvision.transforms.functional.pad
-    Allows passing the pad function to torchvision.transforms.Compose
-    """
-
-    def __init__(self, padding, fill=0, padding_mode='constant'):
-        self.padding = padding
-        self.fill = fill
-        self.padding_mode = padding_mode
-
-    def __call__(self, img):
-        return torchvision.transforms.functional.pad(img, padding=self.padding, fill=self.fill,
-                                                     padding_mode=self.padding_mode)
-
-
-def loadMNIST(root, download, batch_size, num_workers):
-    # pad MNIST 28x28 images with 2 px to reach same size as CIFAR (32x32)
-    transform = torchvision.transforms.Compose([
-        PaddingWrapper(padding=2, padding_mode='edge'),
-        torchvision.transforms.ToTensor()
-    ])
-
-    trainset = torchvision.datasets.MNIST(root=root, train=True, download=download, transform=transform)
-    trainLoader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
-
-    testset = torchvision.datasets.MNIST(root=root, train=False, download=download, transform=transform)
-    testLoader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
-
-    classes = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
-
-    return (trainLoader, testLoader, classes)
-
-
-def loadCIFAR10(root, download, batch_size, num_workers):
-    transform = torchvision.transforms.Compose([
-        torchvision.transforms.ToTensor(),
-        torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-    ])
-
-    trainset = torchvision.datasets.CIFAR10(root=root, train=True, download=download, transform=transform)
-    trainLoader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
-
-    testset = torchvision.datasets.CIFAR10(root=root, train=False, download=download, transform=transform)
-    testLoader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
-
-    classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-
-    return (trainLoader, testLoader, classes)
 
 
 def createNet(model, in_channels=3, num_classes=10):
@@ -111,7 +56,7 @@ def main():
     in_channels = 3 if _dataset == DataSet.CIFAR10 else 1
     num_classes = 10
 
-    dataset_path = '../datasets'
+    dataset_path = '../datasets/'
     download = False
     batch_size = 32
     dataloader_workers = 2
@@ -126,9 +71,9 @@ def main():
     # Load Data
     dataset = None
     if _dataset is DataSet.CIFAR10:
-        dataset = loadCIFAR10(dataset_path, download, batch_size, dataloader_workers)
+        dataset = loadTorchCIFAR10(dataset_path, download, batch_size, dataloader_workers)
     else:
-        dataset = loadMNIST(dataset_path, download, batch_size, dataloader_workers)
+        dataset = loadTorchMNIST(dataset_path, download, batch_size, dataloader_workers)
     trainLoader, testLoader, classes = dataset
 
     # Training
