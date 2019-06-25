ce51b87558aec2f737c8a7fdf7c6dc957e4fcfea
fluegelk <katharina.fluegel@student.kit.edu>
2019-06-25 13:32:47 +0200

    ADD: command line parsing to main
    Takes command line options for all relevant hyper parameters and configurations.

--- implementation/main.py
+++ implementation/main.py
@@ -6,6 +6,8 @@ import random
 import datetime
 import math
 from mpi4py import MPI
+import sys
+import getopt
 
 comm = MPI.COMM_WORLD
 
@@ -75,19 +77,33 @@ def createTraining(trainingType, net, criterion, optimizer, trainLoader, testLoa
     print("Error: invalid training type")
 
 
-def printAccuracy(net, dataset):
-    _, testLoader, classes = dataset
-    totalAccuracy, accuracyByClass = testing.computeAccuracy(net, testLoader, len(classes))
-    print('Total Accuracy: %d %%' % (100 * totalAccuracy))
-    testing.printClassAccuracy(classes, accuracyByClass)
+def train(datasetType, dataset_path, modelType, dataLoaderType, trainingType, max_epochs,
+          max_epochs_without_improvement, learning_rate, momentum, batch_size):
+    # Assign each process a device
+    device = torch.device('cpu')  # default: cpu device
+    # if rank < GPU count use GPU with id 'rank'
+    if comm.Get_rank() < torch.cuda.device_count():
+        device = torch.device('cuda', comm.Get_rank())
 
+    # Load Data
+    path = dataset_path if dataLoaderType is DataLoader.Torch else (dataset_path + datasetType.name + ".hdf5")
+    trainLoader, testLoader, classes = createDataLoaders(dataLoaderType, path,
+                                                         batch_size, device, dataset=datasetType)
 
-def showTestBatch(net, testLoader, classes, prepFunc=None):
-    images, actual = next(iter(testLoader))
-    _, predicted = torch.max(net(images), 1)
-    if prepFunc is not None:
-        images = list(map(prepFunc, images))
-    vis.showImgagesAsGrid(images, vis.actualVsPredictedClass(actual, predicted, classes))
+    # Create net and move to correct device
+    in_channels = 3 if datasetType is DataSet.CIFAR10 else 1
+    num_classes = 10
+    net = createNet(modelType, in_channels, num_classes)
+    net.to(device)
+
+    # Training
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(net.parameters(), lr=learning_rate, momentum=momentum)
+
+    trainObj = createTraining(trainingType, net, criterion, optimizer, trainLoader, testLoader, max_epochs,
+                              max_epochs_without_improvement)
+    trainObj.train()
+    return trainObj
 
 
 def setRandomSeeds(seed=0):
@@ -96,30 +112,88 @@ def setRandomSeeds(seed=0):
     np.random.seed(seed)  # set numpy seed
 
 
-def main():
+def main(argv):
     setRandomSeeds()
     torch.set_num_threads(1)
 
-    # Parameters
-    datasetType = DataSet.MNIST
-    modelType = Model.PyTorchTutorialNet
+    dataset_path = '../datasets/'
+    now = datetime.datetime.now().strftime("%Y-%m-%d--%H-%M")
 
-    dataLoaderType = DataLoader.SequentialHDF5
-    trainingType = Training.Sequential
+    # Default Parameters
+    datasetType = DataSet.MNIST
+    modelType = Model.LeNet5Updated
+    dataLoaderType = DataLoader.ParallelHDF5
+    trainingType = Training.AllReduce
 
     max_epochs = 200
     max_epochs_without_improvement = 10
     learning_rate = 0.001
     momentum = 0.9
+    batch_size = 64
 
-    mini_batch_size_per_node = 32
-    batch_size = mini_batch_size_per_node * comm.Get_size()
+    # Define command line options and help text
+    short_opts = "hd:m:l:t:e:b:"
+    long_opts = ["help", "data", "model", "net", "dl", "dataloader", "training",
+                 "epochs", "earlystopping", "learningrate", "momentum", "bs", "batchsize"]
 
-    dataset_path = '../datasets/'
-    in_channels = 3 if datasetType is DataSet.CIFAR10 else 1
-    num_classes = 10
+    help_text = """Train a neural network on an image classification problem (MNIST or CIFAR10) and collect
+running times, errors and losses over time. Results are stored in '../outputs/'.
 
-    now = datetime.datetime.now().strftime("%Y-%m-%d--%H-%M")
+Options:
+    -h --help               Show this screen.
+
+    -d --data               Select data set to train on
+                            values: MNIST, CIFAR10 [default:MNIST]
+
+    -m --model --net        Select model to train, values:
+                            PyTorchTutorialNet, LeNet5, LeNet5Updated, AlexNet
+                            [default:LeNet5Updated]
+
+    -l --dl --dataloader    Select the data loader, values:
+                            SequentialHDF5, ParallelHDF5, Torch [default:ParallelHDF5]
+
+    -t --training           Select the training, values:
+                            Sequential, AllReduce [default:AllReduce]
+
+    -b --bs --batchsize     Total batch size (over all processes) [default:64]
+
+    -e --epochs             Maximum number of training epochs [default:200]
+
+    --earlystopping         Maximum number of epochs without improvement
+                            before the training is stopped [default:10]
+
+    --learningrate          Learning rate [default:0.001]
+
+    --momentum              Training momentum [default:0.9]"""
+
+    # Parse command line arguments
+    try:
+        opts, args = getopt.getopt(argv, short_opts, long_opts)
+    except getopt.GetoptError:
+        print(help_text)
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            print(help_text)
+            sys.exit()
+        elif opt in ("-d", "--data"):
+            datasetType = DataSet[arg]
+        elif opt in ("-m", "--model", "--net"):
+            modelType = Model[arg]
+        elif opt in ("-l", "--dl", "--dataloader"):
+            dataLoaderType = DataLoader[arg]
+        elif opt in ("-t", "--training"):
+            trainingType = Training[arg]
+        elif opt in ("-b", "--bs", "--batchsize"):
+            batch_size = int(arg)
+        elif opt in ("-e", "--epochs"):
+            max_epochs = int(arg)
+        elif opt in ("--earlystopping"):
+            max_epochs_without_improvement = int(arg)
+        elif opt in ("--learningrate"):
+            learning_rate = float(arg)
+        elif opt in ("--momentum"):
+            momentum = float(arg)
 
     config = {
         "dataset": datasetType.name,
@@ -134,34 +208,20 @@ def main():
         "date": now}
     metadata = "\n# " + str(config)
 
+    print('Configuration:')
+    for opt, value in config.items():
+        print("  {:<31}  {}".format(opt + ':', value))
+    print('')
+
     if trainingType == Training.Sequential and comm.Get_rank() != 0:
         return
 
-    # Assign each process a device
-    device = torch.device('cpu')  # default: cpu device
-    # if rank < GPU count use GPU with id 'rank'
-    if comm.Get_rank() < torch.cuda.device_count():
-        device = torch.device('cuda', comm.Get_rank())
-
-    # Load Data
-    path = dataset_path if dataLoaderType is DataLoader.Torch else (dataset_path + datasetType.name + ".hdf5")
-    trainLoader, testLoader, classes = createDataLoaders(dataLoaderType, path, batch_size, device, dataset=datasetType)
-
-    # Create net and move to correct device
-    net = createNet(modelType, in_channels, num_classes)
-    net.to(device)
-
-    # Training
-    criterion = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(net.parameters(), lr=learning_rate, momentum=momentum)
-
-    trainObj = createTraining(trainingType, net, criterion, optimizer, trainLoader, testLoader, max_epochs,
-                              max_epochs_without_improvement)
-    trainObj.train()
+    trainObj = train(datasetType, dataset_path, modelType, dataLoaderType, trainingType, max_epochs,
+                     max_epochs_without_improvement, learning_rate, momentum, batch_size)
 
     if comm.Get_rank() == 0:
         trainObj.saveResults("outputs/results__" + now, comment=metadata)
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
