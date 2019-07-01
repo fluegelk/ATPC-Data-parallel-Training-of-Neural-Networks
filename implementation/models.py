f58161a0b89ddeef4f6aa77bcaaf3072af8cb36d
fluegelk <katharina.fluegel@student.kit.edu>
2019-07-01 21:26:42 +0200

    EDIT: Reactivate 1st and 2nd max pooling layer in AlexNet

--- implementation/models.py
+++ implementation/models.py
@@ -81,7 +81,7 @@ class AlexNet(nn.Module):
     Changes:
     - fix position of dropout in classifier (ref. https://github.com/pytorch/vision/issues/549)
     - add variable number of input channels (for MNIST dataset)
-    - remove max pooling layers in features to avoid reducing the inputs to size 0
+    - remove last max pooling layer in features to avoid reducing the inputs to size 0
     """
 
     def __init__(self, in_channels=3, num_classes=1000):
@@ -90,10 +90,10 @@ class AlexNet(nn.Module):
         self.features = nn.Sequential(
             nn.Conv2d(in_channels, 64, kernel_size=11, stride=4, padding=2),
             nn.ReLU(inplace=True),
-            # nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.MaxPool2d(kernel_size=3, stride=2),
             nn.Conv2d(64, 192, kernel_size=5, padding=2),
             nn.ReLU(inplace=True),
-            # nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.MaxPool2d(kernel_size=3, stride=2),
             nn.Conv2d(192, 384, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.Conv2d(384, 256, kernel_size=3, padding=1),
